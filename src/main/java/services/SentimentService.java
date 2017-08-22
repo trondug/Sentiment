c4fd993c07618abe2ce2be17ad6c8fcf97634f7e
nilstes <nils.tesdal@gmail.com>
2017-08-22 10:40:15 +0200

    Tatt bort ES-logging og oppdatert versjoner

--- src/main/java/services/SentimentService.java
+++ src/main/java/services/SentimentService.java
@@ -1,15 +1,14 @@
 package services;
 
 import analysis.Analyser;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import logger.RequestLogger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Service that analyzes text sentiment
@@ -20,17 +19,16 @@ public class SentimentService {
     
     private static final Logger log = Logger.getLogger(SentimentService.class.getName());
 
-    private Analyser analyser = new Analyser();
-    private RequestLogger requestLogger = new RequestLogger();
-    
+    private static Analyser analyser = new Analyser();
+
     @POST
     @Consumes("text/plain")
     @Produces("text/plain")
-    public int getSentiment(@Context HttpServletRequest req, String text) { 
-        log.log(Level.INFO, "GameService.getSentiment(): {0}", text);
+    public int getSentiment(@Context HttpServletRequest req, String text) {
+        String userAgent = req.getHeader("User-Agent");
+        log.log(Level.INFO, "SentimentService.getSentiment(): text={0}, userAgent={1}", new Object[] {text, userAgent});
         int sentiment = analyser.findSentiment(text);
-        requestLogger.logRequest(text, sentiment, req);
-        log.log(Level.INFO, "Returning {0}", sentiment);      
+        log.log(Level.INFO, "Returning {0}", sentiment);
         return sentiment;
     }    
 }
